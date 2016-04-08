d6a092b756b250878c55c35274d255c3d47d4625
Matthew Francis <matthew_francis@outlook.com>
2016-04-08 15:02:34 -0400

    improved map functionality

--- public/js/app.js
+++ public/js/app.js
@@ -17,36 +17,53 @@ angular.module("contactsApp", ['ngRoute'])
 	})
 })
 .controller('MapController', ['$scope', '$http', function($scope, $http) {
-
+    var geocoder = new google.maps.Geocoder;
+    //geolocation button
     document.getElementById("geolocate").onclick = function(){
         var geoSuccess = function(position) {
-            mapCoordinates(position);
+            var lat = position.coords.latitude;
+            var lng = position.coords.longitude;
+            mapCoordinates({lat,lng});
         };
         var geoError = function(error) {
             console.log('Error occurred. Error code: ' + error.code);
         };
         navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
     };
-
-
+    
     function mapCoordinates(postition) {
         function initialize() {
-        var myCenter= new google.maps.LatLng(postition.coords.latitude,postition.coords.longitude);
         var mapProp = {
-            center:myCenter,
-            zoom:12,
+            center:postition,
+            zoom:15,
             mapTypeId:google.maps.MapTypeId.ROADMAP
         };
         var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
     
         var marker=new google.maps.Marker({
-            position:myCenter,
+            position:postition,
         });
 
         marker.setMap(map);
     }
-        initialize();
+    initialize();
     }
+    
+    $scope.submit = function(){
+        var address = $scope.destination;
+        function codeAddress() {
+        geocoder.geocode( { 'address': address}, function(results, status) {
+            if (status === google.maps.GeocoderStatus.OK) {
+                mapCoordinates(results[0].geometry.location);
+            } else {
+                alert('Geocode was not successful for the following reason: ' + status);
+            }
+        });
+        }
+        codeAddress();
+    }
+    
+    
 }])
 .controller('LoginController', ['$scope', '$http', function($scope, $http) {
 
